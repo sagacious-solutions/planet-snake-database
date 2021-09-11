e64007087a42cbd591587ecfef8400f729cfab2d
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-11 15:31:27 -0700

    created the enviro_readings table

--- db/schema/02_hardware_state.sql
+++ db/schema/02_hardware_state.sql
@@ -3,3 +3,17 @@ CREATE TABLE hardware_state (
   id SERIAL PRIMARY KEY NOT NULL,
   time_created TIMESTAMP DEFAULT now(), 
 );
+
+DROP TABLE IF EXISTS enviro_readings;
+CREATE TABLE enviro_readings (
+    id SERIAL PRIMARY NOT NULL,
+    hardware_state_id INTEGER REFERENCES hardware_state(id), -- ON DELETE CASCADE,
+    sensor_id INTEGER REFERENCES sensor(id),
+    temperature REAL,
+    humidity REAL
+);
+
+DROP TABLE IF EXISTS sensor;
+CREATE TABLE sensor (
+
+);
