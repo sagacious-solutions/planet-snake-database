54b3cac77a188a7071319478441be983245350ff
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-11 15:02:22 -0700

    fixed typo

--- db/schema/01_snake_state.sql
+++ db/schema/01_snake_state.sql
@@ -1,7 +1,7 @@
 -- Drop and recreate Users table (Example)
 
-
-CREATE TABLE creator (
+DROP TABLE IF EXISTS snake_state
+CREATE TABLE snake_state (
   id SERIAL PRIMARY KEY NOT NULL,
   time_created TIMESTAMP DEFAULT now(), 
   time_light_switch SMALLINT CHECK (time_light_switch > 0 AND time_light_switch < 13),
@@ -12,6 +12,6 @@ CREATE TABLE creator (
   rat_offered BOOLEAN DEFAULT "FALSE",
   rat_ate BOOLEAN DEFAULT "FALSE",
   rat_ignored BOOLEAN DEFAULT "FALSE",
-  snake_length SMALLINT CHECK (snake_length > 0)
+  snake_length SMALLINT CHECK (snake_length > 0),
   snake_weight SMALLINT CHECK (snake_weight > 0)
 );
