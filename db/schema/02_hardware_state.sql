9cab7a5bde989e18a0e7a6f757bab8de1c8b3a88
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-11 15:54:05 -0700

    completed building table

--- db/schema/02_hardware_state.sql
+++ db/schema/02_hardware_state.sql
@@ -6,7 +6,7 @@ CREATE TABLE hardware_state (
 
 DROP TABLE IF EXISTS enviro_readings;
 CREATE TABLE enviro_readings (
-    id SERIAL PRIMARY NOT NULL,
+    id BIGSERIAL PRIMARY NOT NULL,
     hardware_state_id INTEGER REFERENCES hardware_state(id), -- ON DELETE CASCADE,
     sensor_id INTEGER REFERENCES sensor(id),
     temperature REAL,
@@ -15,5 +15,26 @@ CREATE TABLE enviro_readings (
 
 DROP TABLE IF EXISTS sensor;
 CREATE TABLE sensor (
+    id SERIAL PRIMARY NOT NULL,
+    sensor_id VARCHAR(15)
+    sensor_model VARCHAR(15),
+    description TEXT,
+    time_created TIMESTAMP DEFAULT now(), 
 
+);
+
+DROP TABLE IF EXISTS heater_state;
+CREATE TABLE heater_state (
+    id SERIAL PRIMARY NOT NULL,
+    hardware_state_id INTEGER REFERENCES hardware_state(id),
+    heater_id INTEGER REFERENCES heater(id),
+    is_turned_on BOOLEAN NOT NULL,
+);
+
+DROP TABLE IF EXISTS heater;
+CREATE TABLE heater (
+    id SERIAL PRIMARY NOT NULL,
+    socket_num SMALLINT,
+    description TEXT,
+    time_created TIMESTAMP DEFAULT now(), 
 );
