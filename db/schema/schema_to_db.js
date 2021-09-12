eaebb7d5469a91490e2a2322ead07b011230c896
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-12 10:04:34 -0700

    changed error messaging

--- db/schema/schema_to_db.js
+++ db/schema/schema_to_db.js
@@ -1,25 +1,20 @@
 const { db_client } = require("../db.js");
 const fs = require("fs");
 
-let sql_file = "./db/schema/02_polls.sql";
+let sql_file = "./db/schema/01_snake_state.sql";
 
 // This will write the seeds file to our database
 //fs.readFile("./db/seeds/02_polls_seed.sql",'utf-8', function (err, data) {
-fs.readFile(
-    sql_file,
-    "utf-8",
-    function (err, data) {
-      if (data) {
-        db_client
-          .query(data)
-          .then(()=>{
-            console.log('Sucessfully created tables from', sql_file);
-          })
-          .catch((err)=>console.log('god fucking damnit ', err))
-          .then(()=>{
-            db_client.end();
-          });
-      }
-    }
-);
-
+fs.readFile(sql_file, "utf-8", function (err, data) {
+  if (data) {
+    db_client
+      .query(data)
+      .then(() => {
+        console.log("Sucessfully created tables from", sql_file);
+      })
+      .catch((err) => console.log("Something went wrong : ", err))
+      .then(() => {
+        db_client.end();
+      });
+  }
+});
