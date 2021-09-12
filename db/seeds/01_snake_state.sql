abc8cb57832bc641faaedcd43f402180f2ac1dd6
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-12 10:48:25 -0700

    added more poops found for testing

--- db/seeds/01_snake_state.sql
+++ db/seeds/01_snake_state.sql
@@ -3,8 +3,17 @@
 -- INSERT INTO users (name) VALUES ('Kira');
 
 INSERT INTO snake_state (time_created, time_light_switch, shed_iminent) VALUES ((NOW() - INTERVAL '30 days'),7, 't');
-INSERT INTO snake_state (time_created, time_light_switch, poop_found) VALUES (NOW()-INTERVAL'60 days',7, 't');
 INSERT INTO snake_state (time_created, time_light_switch, rat_offered) VALUES (NOW()- INTERVAL'1 days',7, 't');
 INSERT INTO snake_state (time_created, time_light_switch, snake_length) VALUES (NOW()- INTERVAL'100 days',7, 50);
 INSERT INTO snake_state (time_created, time_light_switch, snake_weight) VALUES (NOW()- INTERVAL'90 days',7, 10);
 INSERT INTO snake_state (time_created, time_light_switch, urate_found) VALUES (NOW()- INTERVAL'20 days',7, 't');
+
+
+
+
+
+INSERT INTO snake_state (time_created, time_light_switch, poop_found) VALUES (NOW()-INTERVAL'14 days',7, 't');
+INSERT INTO snake_state (time_created, time_light_switch, poop_found) VALUES (NOW()-INTERVAL'99 days',7, 't');
+INSERT INTO snake_state (time_created, time_light_switch, poop_found) VALUES (NOW()-INTERVAL'37 days',7, 't');
+INSERT INTO snake_state (time_created, time_light_switch, poop_found) VALUES (NOW()-INTERVAL'60 days',7, 't');
+INSERT INTO snake_state (time_created, time_light_switch, poop_found) VALUES (NOW()-INTERVAL'115 days',7, 't');
