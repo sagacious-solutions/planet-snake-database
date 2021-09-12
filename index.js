c0e13a3170c1d80d586cac7776e2fe347e08227a
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-12 09:11:41 -0700

    removed body parser and added router

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const PORT = process.env.PORT || 8080;
-const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
 const app = express();
+
+app.use(morgan("dev"));
+
+const pageRoutes = require("./routes/redirects");
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
