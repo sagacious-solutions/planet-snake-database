77241d0af0fa1ee250c83e8a9b40e54ae1b724c8
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-12 10:04:14 -0700

    fixed syntax issue

--- db/schema/01_snake_state.sql
+++ db/schema/01_snake_state.sql
@@ -5,13 +5,13 @@ CREATE TABLE snake_state (
   id SERIAL PRIMARY KEY NOT NULL,
   time_created TIMESTAMP DEFAULT now(), 
   time_light_switch SMALLINT CHECK (time_light_switch > 0 AND time_light_switch < 13),
-  shed_iminent BOOLEAN DEFAULT "FALSE",
-  shed_complete BOOLEAN DEFAULT "FALSE",
-  poop_found BOOLEAN DEFAULT "FALSE",
-  urate_found BOOLEAN DEFAULT "FALSE",
-  rat_offered BOOLEAN DEFAULT "FALSE",
-  rat_ate BOOLEAN DEFAULT "FALSE",
-  rat_ignored BOOLEAN DEFAULT "FALSE",
-  snake_length SMALLINT CHECK (snake_length > 0),
-  snake_weight SMALLINT CHECK (snake_weight > 0)
+  shed_iminent BOOLEAN DEFAULT 'f',
+  shed_complete BOOLEAN DEFAULT 'f',
+  poop_found BOOLEAN DEFAULT 'f',
+  urate_found BOOLEAN DEFAULT 'f',
+  rat_offered BOOLEAN DEFAULT 'f',
+  rat_ate BOOLEAN DEFAULT 'f',
+  rat_ignored BOOLEAN DEFAULT 'f',
+  snake_length SMALLINT CHECK (snake_length > 0) ,
+  snake_weight SMALLINT CHECK (snake_weight > 0) 
 );
