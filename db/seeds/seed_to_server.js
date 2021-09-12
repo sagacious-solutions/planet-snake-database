38fe0c77e001f00db094eb99542eec89dda16207
Matt Macdonald <mksmacdonald@gmail.com>
2021-09-12 10:05:31 -0700

    changed to use correct seed file

--- db/seeds/seed_to_server.js
+++ db/seeds/seed_to_server.js
@@ -1,28 +1,23 @@
 const { db_client } = require("../db.js");
 const fs = require("fs");
 
-let sql_file = "./db/seeds/02_polls_seed.sql";
+let sql_file = "./db/seeds/01_snake_state.sql";
 
 // This will write the seeds file to our database
 //fs.readFile("./db/seeds/02_polls_seed.sql",'utf-8', function (err, data) {
-fs.readFile(
-    sql_file,
-    "utf-8",
-    function (err, data) {
-      if (data) {        
-        db_client
-          .query(data)
-          .then(()=>{            
-            console.log("Succesfully created seed data.");
-            db_client.end();
-            console.log('connection to database terminated');
-          })
-          .catch((err)=>{
-            console.log(`unable to create seed`, err);
-            db_client.end();
-            console.log('connection to database terminated');
-          });
-      }
-    }
-);
-
+fs.readFile(sql_file, "utf-8", function (err, data) {
+  if (data) {
+    db_client
+      .query(data)
+      .then(() => {
+        console.log("Succesfully created seed data.");
+        db_client.end();
+        console.log("connection to database terminated");
+      })
+      .catch((err) => {
+        console.log(`unable to create seed`, err);
+        db_client.end();
+        console.log("connection to database terminated");
+      });
+  }
+});
