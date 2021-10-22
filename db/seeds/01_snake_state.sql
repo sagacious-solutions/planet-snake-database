27876acec8250fd19ae45c3870672806758428ed
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-22 09:06:37 -0700

    removed comments

--- db/seeds/01_snake_state.sql
+++ db/seeds/01_snake_state.sql
@@ -1,7 +1,3 @@
--- Users table seeds here (Example)
--- INSERT INTO users (name) VALUES ('Alice');
--- INSERT INTO users (name) VALUES ('Kira');
-
 INSERT INTO snake_state (time_created, time_light_switch, shed_imminent) VALUES ((NOW() - INTERVAL '20 days'),7, 't');
 INSERT INTO snake_state (time_created, time_light_switch, shed_complete) VALUES ((NOW() - INTERVAL '4 days'),7, 't');
 INSERT INTO snake_state (time_created, time_light_switch, rat_offered) VALUES (NOW()- INTERVAL'15 days',7, 't');
