b27b44d16a892fd3f79a2b91be18b63d45eb9a23
Matt Macdonald <mksmacdonald@gmail.com>
2021-10-22 09:07:21 -0700

    added timeout to get graceful exit

--- db/schema/schema_to_db.js
+++ db/schema/schema_to_db.js
@@ -6,15 +6,17 @@ let sql_file = "./db/schema/01_snake_state.sql";
 // This will write the seeds file to our database
 //fs.readFile("./db/seeds/02_polls_seed.sql",'utf-8', function (err, data) {
 fs.readFile(sql_file, "utf-8", function (err, data) {
+  const ONE_SECOND_MS = 1000;
+
   if (data) {
     db_client
       .query(data)
       .then(() => {
         console.log("Sucessfully created tables from", sql_file);
+        setTimeout(() => db_client.end(), ONE_SECOND_MS);
       })
-      .catch((err) => console.log("Something went wrong : ", err))
-      .then(() => {
-        db_client.end();
+      .catch((err) => {
+        console.log("Unable to create tables", err);
       });
   }
 });
--- db/seeds/seed_to_server.js
+++ db/seeds/seed_to_server.js
@@ -3,16 +3,13 @@ const fs = require("fs");
 
 let sql_file = "./db/seeds/01_snake_state.sql";
 
-// This will write the seeds file to our database
-//fs.readFile("./db/seeds/02_polls_seed.sql",'utf-8', function (err, data) {
 fs.readFile(sql_file, "utf-8", function (err, data) {
   if (data) {
     db_client
       .query(data)
       .then(() => {
         console.log("Succesfully created seed data.");
-        db_client.end();
-        console.log("connection to database terminated");
+        endDatabaseConnection();
       })
       .catch((err) => {
         console.log(`unable to create seed`, err);
@@ -21,3 +18,12 @@ fs.readFile(sql_file, "utf-8", function (err, data) {
       });
   }
 });
+
+const endDatabaseConnection = () => {
+  const ONE_SECOND_MS = 1000;
+
+  setTimeout(() => {
+    db_client.end();
+    console.log("connection to database terminated");
+  }, ONE_SECOND_MS);
+};
